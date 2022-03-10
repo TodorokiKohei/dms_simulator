07b7b09db3dd3db31e51768f532f7eb1e1533297
TodorokiKohei <b182814@hiroshima-u.ac.jp>
2022-03-10 09:17:24 +0900

    サンプル追加

--- tests/containers/test_containers.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-
-from libs.containers import KafkaContainer, ZookeeperContainer
-
-
-if __name__ == '__main__':
-    print('test')
