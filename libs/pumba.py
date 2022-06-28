8954762ca1703e62d5e0ce5b1d839e7ad9759668
TodorokiKohei <b182814@hiroshima-u.ac.jp>
2022-06-28 19:24:05 +0900

    コンテナの停止処理を実装

--- libs/pumba.py
+++ libs/pumba.py
@@ -18,6 +18,28 @@ class TcContainer(Container):
 
 
 class PumbaContainer(Container):
+    def __init__(self, name: str, configs: dict, target_container: str):
+        configs['image'] = 'gaiaadm/pumba'
+        configs['volumes'] = [
+            {
+                'type': 'bind',
+                'source': '/var/run/docker.sock',
+                'target': '/var/run/docker.sock'
+            }
+        ]
+        self.target_container = target_container
+        self._mode = configs.pop('mode')
+        self._start = configs.pop('start')
+        super().__init__(name, **configs)
+
+    @property
+    def start(self):
+        return utils.change_time_to_sec(self._start)
+
+    @property
+    def mode(self):
+        return self._mode
+
     def to_swarm(self):
         swarm = super().to_swarm()
         restart_policy = {
@@ -37,25 +59,13 @@ class PumbaContainer(Container):
 
 class PumbaNetemContainer(PumbaContainer):
     def __init__(self, name: str, configs: dict, target_container: str):
-        configs['image'] = 'gaiaadm/pumba'
-        configs['volumes'] = [
-            {
-                'type': 'bind',
-                'source': '/var/run/docker.sock',
-                'target': '/var/run/docker.sock'
-            }
-        ]
-        self.target_container = target_container
-        self._mode = configs.pop('mode')
-        self._start = configs.pop('start')
         self._duration = configs.pop('duration')
         self._destination = configs.pop('destination', [])
         self._params = configs.pop('params', {})
-        super().__init__(name, **configs)
+        super().__init__(name, configs, target_container)
 
-    @property
-    def start(self):
-        return utils.change_time_to_sec(self._start)
+    def is_netem(self):
+        return True
 
     def create_commnad(self, containers: List[Container]):
         cmd = f'--json --log-level info netem -d {self._duration} --tc-image gaiadocker/iproute2'
@@ -67,6 +77,22 @@ class PumbaNetemContainer(PumbaContainer):
                     is_found = True
             if not is_found:
                 raise RuntimeError(f"Container {dest} is not found")
+        cmd += f' {self._mode}'
+        for param, value in self._params.items():
+            cmd += f' --{param} {value}'
+        cmd += f' re2:.*{self.target_container}.*'
+        self.command = cmd
+
+class PumbaCrashContainer(PumbaContainer):
+    def __init__(self, name: str, configs: dict, target_container: str):
+        self._params = configs.pop('params', {})
+        super().__init__(name, configs, target_container)
+
+    def is_netem(self):
+        return False
+
+    def create_commnad(self, containers: List[Container]):
+        cmd = f'--json --log-level info'
         cmd += f' {self._mode}'
         for param, value in self._params.items():
             cmd += f' --{param} {value}'
